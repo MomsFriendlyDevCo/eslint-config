93d937b7f8280775070d39c1f4fd6e9cca7a9916
Matt Carter <m@ttcarter.com>
2024-07-23 17:58:03 +1000

    fix: More JSDoc rules

--- index.js
+++ index.js
@@ -4,17 +4,45 @@ import pluginVue from 'eslint-plugin-vue';
 
 // Rules shared by both .doop + .vue files
 let jsCommonRules = {
+	// Generic rules
 	'html-closing-bracket-spacing': ['off'], // Annoying doesn't allow <this-kind-of-thing/>
-	'jsdoc/check-alignment': ['off'],  // Disable the JSDoc parser insisting on correct indents
-	'jsdoc/check-types': ['off'], // Disable the JSDoc parser being fussy about `@param {String}` vs `@param {string}`
-	'jsdoc/tag-lines': ['off'], // Disable the JSDoc parser being fussy about new-line spacing
 	'no-debugger': ['warn'], // Debuggers are fine, just warn
 	'no-useless-escape': ['off'], // ESlint frequently gets what should and shouldn't be escaped wrong
 	'no-unused-vars': ['warn'], // Dont make unused vars the end of the world
+
+	// JSDoc
+	'jsdoc/check-alignment': ['off'],  // Disable the JSDoc parser insisting on correct indents
+	'jsdoc/check-tag-names': ['warn', { // Extend JSDoc allowed tags
+		definedTags: ['date', 'url'],
+	}],
+	'jsdoc/check-types': ['off'], // Disable the JSDoc parser being fussy about `@param {String}` vs `@param {string}`
+	'jsdoc/no-defaults': ['off'], // Disable the JSDoc parser that complains about optional params as its just silly
+	'jsdoc/no-undefined-types': ['warn', {disableReporting: true}], // Disable the JSDoc parser for "undefined" types which it usually gets wrong. Ask eslint to still mark these as used though
+	'jsdoc/require-returns-description': ['off'], // Disable the JSDoc parse being fussy about return descriptions
+	'jsdoc/tag-lines': ['off'], // Disable the JSDoc parser being fussy about new-line spacing
+
+	// Vue
+	'vue/no-v-html': ['off'], // Disable Vue complaining about v-html being used
 };
 
 export default [
-	pluginJSDoc.configs['flat/recommended'],
+	// eslint-plugin-jsdoc (with custom settings)
+	{
+		...pluginJSDoc.configs['flat/recommended'],
+		settings: {
+			jsdoc: {
+				mode: 'jsdoc',
+				tagNamePreference: {
+					// Prefer 'emits' over 'fires' to match Vue terminology {{{
+					emits: 'emits',
+					fires: 'emits',
+					// }}}
+				},
+			},
+		},
+	},
+
+	// eslint-plugin-vue
 	...pluginVue.configs['flat/recommended'],
 
 	// .doop backend Files {{{
