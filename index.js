984a29a800856341f1935b7edbe6cd5b399e507f
Matt Carter <m@ttcarter.com>
2026-08-18 14:43:32 +1000

    task: Updated 3rd party rule libraries

--- index.js
+++ index.js
@@ -97,12 +97,14 @@ export default [
 			'unicorn/no-array-reduce': ['off'],
 			'unicorn/no-await-expression-member': ['off'], // There is nothing wrong with `(await thing).prop`
 			'unicorn/no-empty-file': ['off'], // Breaks interface files + schema files with just JSDoc
+			'unicorn/no-for-each': ['off'], // Utterly wrong when chaining array handling
 			'unicorn/no-magic-array-flat-depth': ['warn'],
 			'unicorn/no-negated-condition': ['off'],
 			'unicorn/no-nested-ternary': ['off'],
 			'unicorn/no-null': ['off'],
 			'unicorn/no-this-assignment': ['off'], // Not allowing escape hatches to `this` when nesting functions is just dumb
 			'unicorn/no-useless-promise-resolve-reject': ['off'], // Constant complaints about using `throw` within simple promise chains
+			'unicorn/prefer-await': ['off'], // Utterly wrong when recommending 'await' instead of Promise chains
 			'unicorn/prefer-dom-node-append': ['warn'],
 			'unicorn/prefer-global-this': ['warn'], // Disabled in .vue files to allow 'window' directly
 			'unicorn/prefer-logical-operator-over-ternary': ['off'],
@@ -110,7 +112,6 @@ export default [
 			'unicorn/prefer-string-raw': ['off'], // Just a downright weird rule
 			'unicorn/prefer-string-replace-all': ['off'], // Gets annoying pretty fast and its rarely correct
 			'unicorn/prefer-ternary': ['off'], // This rule means well but it can screw up perfectly readable code
-			'unicorn/prefer-ternary': ['warn'],
 			'unicorn/prefer-type-error': ['off'], // Too TS specific
 			'unicorn/prevent-abbreviations': ['off'],
 			'unicorn/switch-case-braces': ['warn', 'avoid'],
